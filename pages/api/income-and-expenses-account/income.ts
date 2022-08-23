f9ec6af9a526f072a7b6fa7d6bb67915e81d7e51
Toktak Mongkhon <mongkhon@rentspree.com>
2022-08-24 02:12:40 +0700

    add expense and income endpoint

---
+++ pages/api/income-and-expenses-account/income.ts
@@ -0,0 +1,23 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { ErrorType } from "../../../interface/error-type";
+import { IGetIncomeAndExpensesAccountOutputType } from "../../../interface/income-and-expenses-account";
+import { Transaction } from "../../../interface/transaction";
+import { createIncomeService } from "../../../service";
+import errorParser from "../../../utils/error-parser";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Transaction | ErrorType>
+) {
+  try {
+    const response = await createIncomeService(
+      req.body,
+      req.headers["authorization"]
+    );
+    res.status(200).json(response);
+  } catch (err) {
+    const parseError = errorParser(err);
+    return res.status(parseError.status).send(parseError);
+  }
+}
